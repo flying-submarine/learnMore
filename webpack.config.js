15456382df0a0e7ac53369f558d3ce0bda7c322c
submarine <532384048@qq.com>
2022-07-05 16:46:33 +0800

    热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
   entry: {
     index:'./src/index.js',
     search:'./src/search.js',
-
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -58,6 +57,13 @@ module.exports = {
       }
     ]
   },
-  mode : "production",
-  plugins:[]
+  mode : "development",
+  plugins:[
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer:{
+    static: './dist',
+    // contentBase : "./dist",
+    hot:true
+  }
 };

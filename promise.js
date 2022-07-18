bfb9b1937184da218c0515e390f381dd25a40cbe
submarine <532384048@qq.com>
2022-07-18 23:46:15 +0800

    初始化

---
+++ promise.js
@@ -0,0 +1,37 @@
+function PromiseDemo(fn){
+    const _this = this
+    _this.prmState = "pending"
+    _this.prmValue = undefined
+    const resolve = (value)=>{
+        if(_this.prmState== "pending"){
+            _this. prmState = "fullfilled"
+            _this.promiseValue = value
+        }
+    }
+    const reject = (value)=>{
+        if(_this.prmState== "pending"){
+            _this. prmState = "rejected"
+            _this.promiseValue = value
+        }  
+    }
+
+    if(fn) {fn(resolve,reject)} else {
+        throw("Init Error,Please use a function to init promiseDemo!")
+    }
+}
+
+PromiseDemo.prototype.then = (callback)=>{
+}
+
+PromiseDemo.prototype.catch = (callback)=>{
+}
+
+var p = new PromiseDemo(function(resolve,reject){
+    resolve(123)
+})
+console.log(p,'xxx') 
+ p.then(function(res){
+    console.log(res)
+})
+
+

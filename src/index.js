0d621715b74bb67cbfa6b4915759157801fbdc98
submarine <532384048@qq.com>
2022-07-18 22:30:59 +0800

    清空

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Menu from "./menu"
-// function providePlugin(){
-//     const arr = ["jiash2222222232eng","zh4oujun123","hengyuan3456"]
-//     arr.map(i=>console.log(i))
-//  };
-// providePlugin()
-window.onload = function(){
-    Menu()
-}
--- src/menu.js
+++
@@ -1,3 +0,0 @@
-export default function menu(){
-    console.log("menu1")
-}

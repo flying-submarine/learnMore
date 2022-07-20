c37d0ae1d3ba9bb10c8db639f7c54d7adde0f73d
submarine <532384048@qq.com>
2022-07-20 21:29:49 +0800

    实现then

--- promise.js
+++ promise.js
@@ -2,25 +2,29 @@ function PromiseDemo(fn){
     this.prmState = "pending"
     this.prmValue = undefined
     this.thenCallback = undefined
+    var _this = this // 保存this
+
     const resolve = function(value){
-        if(this.prmState== "pending"){
-            this. prmState = "fullfilled"
-            this.prmValue = value
+        if(_this.prmState== "pending"){
+            _this. prmState = "fullfilled"
+            _this.prmValue = value
             setTimeout(()=>{
-                if(this.thenCallback){
-                    this.thenCallback(value)
+                if(_this.thenCallback){
+                    _this.thenCallback(value)
                 }
-            },0)
+            })
         }
     }
     const reject = function(value){
-        if(this.prmState== "pending"){
-            this. prmState = "rejected"
-            this.prmValue = value
+        if(_this.prmState== "pending"){
+            _this. prmState = "rejected"
+            _this.prmValue = value
         }  
     }
 
-    if(fn) {fn(resolve,reject)} else {
+    if(fn) {
+        fn(resolve,reject)
+    } else {
         throw("Init Error,Please use a function to init promiseDemo!")
     }
 }
@@ -35,11 +39,11 @@ PromiseDemo.prototype.catch = (callback)=>{
 }
 
 var p = new PromiseDemo(function(resolve,reject){
-    resolve(123)
+    resolve("resolve")
 })
-console.log(p,'xxx') 
- p.then(function(res){
-    console.log(res)
+console.log(p)
+p.then(function(res){
+    console.log(res,"cc3")
 })
 
 

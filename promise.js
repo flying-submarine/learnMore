004fa840343ebf95c0dadb57b4d9df048df66d87
submarine <532384048@qq.com>
2022-08-01 16:35:50 +0800

    then done

--- promise.js
+++ promise.js
@@ -2,12 +2,14 @@ function PromiseDemo(fn){
     this.prmState = "pending"
     this.prmValue = undefined
     this.thenCallback = undefined
+    this.catchCallback = undefined
     var _this = this // 保存this
 
     const resolve = function(value){
         if(_this.prmState== "pending"){
             _this. prmState = "fullfilled"
             _this.prmValue = value
+            //当传⼊的类型是Promise对象时
             if (value instanceof PromiseDemo ){
                 value.then(function(res){
                     _this.thenCallback(res)
@@ -22,10 +24,15 @@ function PromiseDemo(fn){
             }
         }
     }
-    const reject = function(value){
+    const reject = function(err){
         if(_this.prmState== "pending"){
             _this. prmState = "rejected"
-            _this.prmValue = value
+            _this.prmValue = err
+            setTimeout(()=>{
+                if(_this.catchCallback){
+                    _this.catchCallback(value)
+                }
+            })
         }  
     }
 
@@ -37,7 +44,7 @@ function PromiseDemo(fn){
 }
 
 PromiseDemo.prototype.then = function(callback){
-    var _this = this
+    var _this = this // ?
     return new PromiseDemo(function(resolve,reject){
         _this.thenCallback = function(value){
             var callbackRes = callback(value)
@@ -46,17 +53,28 @@ PromiseDemo.prototype.then = function(callback){
     })
 }
 
-PromiseDemo.prototype.catch = (callback)=>{
+PromiseDemo.prototype.catch = function(callback){
+    var _this = this
+    return new MyPromise(function(resolve,reject){
+        _this.catchCallback = function(errValue){
+            var callbackRes = callback(errValue)
+            resolve(callbackRes)
+    }
+ })
 }
 
-var p = new PromiseDemo(function(resolve,reject){
-    resolve("resolve")
+var p = new PromiseDemo(function(resolve){
+    resolve(new PromiseDemo(function(resolve1){
+        resolve1('aaa')
+    }))
 })
-console.log(p)
+console.log(p.__proto__,'mm')
 p.then(function(res){
     console.log(res,"cc3")
 }).then(function(res){
     console.log(res,"cc")
+}).then(function(res){
+    console.log(res,"cc1")
 })
 
 
